428f35fa00093fabb0adca242576e147afee9008
이준상 <samgashyeong@naver.com>
2021-11-12 14:56:29 +0000

    api 구현

--- api/views.py
+++ api/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import Quiz
+from .serializer import QuizSerializer
+
+
+@api_view(['GET'])
+def helloapi(request):
+    return Response("helloworld")
 
 # Create your views here.
