a98e93307cbb32d3dc6435bb6eb5be162e7218ce
samgashyeong <samgashyeong@naver.com>
2021-11-25 10:46:17 +0900

    heroku

--- not_missing/settings.py
+++ not_missing/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 
+import dj_database_url
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '8epi(2+cj)e!foq=j2j(^2qsa&&91t@g%b*wu28sw*o67nc2nj'
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", '8epi(2+cj)e!foq=j2j(^2qsa&&91t@g%b*wu28sw*o67nc2nj')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
